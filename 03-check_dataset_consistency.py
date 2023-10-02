983b990102ee60d9c641b36a123bf8d4de5ae9e9
Tomas Delvechio <tomasdelvechio17+asuka@gmail.com>
2023-10-02 12:17:47 -0300

    planteo del control del dataset

---
+++ 03-check_dataset_consistency.py
@@ -0,0 +1,11 @@
+"""
+Este script repara todas las inconsistencias que aparezcan en los datasets
+generados por los scripts 01- y 02-.
+"""
+
+# Para cada dataset
+#   - Verificar que no existan items repetidos
+
+# Para el dataset de usuarios
+#   - Tomar el archivo output/02-users/users_extra_data.csv y agregarlo como columna
+#       al csv de usuarios.
