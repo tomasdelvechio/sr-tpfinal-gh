d0c9c5e035886e4ec8b64e2f1aeac186deb1d6c7
Tomas Delvechio <tomasdelvechio17+asuka@gmail.com>
2023-10-11 00:16:38 -0300

    se comienza script 04

--- run.sh
+++ run.sh
@@ -14,3 +14,8 @@ mkdir -p output/03-check/
 python make_backup.py datasets/
 python make_backup.py output/03-check/
 python 03-check_dataset_consistency.py &> output/03-check/output.log
+
+mkdir -p output/04-extra-data/
+python make_backup.py datasets/
+python make_backup.py output/04-extra-data/
+python 04-users_extra_data.py &> output/04-extra-data/output.log
